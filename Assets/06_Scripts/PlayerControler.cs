abe84eb2fe7f824e3160ded698c3880db23cf7a6
jjjjjjjiwon <hgw7501@gmail.com>
2025-09-01 15:50:55 +0900

    0901

--- Assets/06_Scripts/PlayerControler.cs
+++ Assets/06_Scripts/PlayerControler.cs
@@ -1,21 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+//Create Input Actions for player character movement 여기부터
+
 
 public class PlayerControler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+        float vertical = 0.0f;
+
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+
+        Debug.Log(horizontal);
+
+        Debug.Log(vertical);
+
+
         Vector2 position = transform.position;
-        position.x += 0.1f;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
-           
     }
 }
